8d6ced749d7842f1d237b37ee4eada63b27567c0
chen_jia_jia <chenjiajia@zuzuche.com>
2018-12-14 17:43:28 +0800

    基本算法

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,22 +3,24 @@ module.exports = {
     title: 'andy chen 的博客',
     description: '每天进步一点点',
     themeConfig: {
-        nav: [{
-            text: '更多',
-            items: [
-                { text: '代码片段收集', link: 'https://chen4342024.github.io/code-snippet/' },
-                { text: 'GitHub', link: 'https://github.com/chen4342024' },
-            ]
-        }],
+        nav: [
+            {
+                text: '更多',
+                items: [
+                    {
+                        text: '代码片段收集',
+                        link: 'https://chen4342024.github.io/code-snippet/'
+                    },
+                    { text: 'GitHub', link: 'https://github.com/chen4342024' }
+                ]
+            }
+        ],
         lastUpdated: '上次更新', // string | boolean
-        sidebar: [{
+        sidebar: [
+            {
                 title: 'JS 相关',
                 collapsable: false,
-                children: [
-                    'js/promise',
-                    'js/compressImage',
-                    'js/task',
-                ]
+                children: ['js/promise', 'js/compressImage', 'js/task']
             },
             {
                 title: 'JS高级编程设计',
@@ -31,16 +33,18 @@ module.exports = {
                     'js/js-book/env',
                     'js/js-book/event',
                     'js/js-book/oop',
-                    'js/js-book/referenceType',
+                    'js/js-book/referenceType'
                 ]
             },
             {
                 title: 'underscore源码解析',
                 collapsable: false,
-                children: [
-                    'js/underscore/restArgs',
-                    'js/underscore/tip',
-                ]
+                children: ['js/underscore/restArgs', 'js/underscore/tip']
+            },
+            {
+                title: '算法',
+                collapsable: false,
+                children: ['js/algorithm/baseSort']
             },
             {
                 title: '其他',
@@ -51,10 +55,10 @@ module.exports = {
                     'other/weixinDebug',
                     'other/highQualityCode',
                     'other/cache',
-                    'other/http',
+                    'other/http'
                 ]
             },
-            'aboutMe/',
+            'aboutMe/'
         ]
     }
 };
